faae6d605fc42868d0851d64acff0d5ede51234b
TadenWErdmann <827165@mystma.org>
2026-09-22 14:04:47 -0500

    2.4

---
+++ Assignments/Unit 2/Animal Survey.py
@@ -0,0 +1,11 @@
+animal = " Your favorite animal is a dog. "
+like = " they are cute and playful, "
+live = " they live worldwide, "
+fact = " They are one of the smartest animals ever, "
+color = " and they come in many colors. "
+input("What is your Favorite Animal?")
+input("Why do you like them?")
+input("Where do they live?")
+input ("What is a fun fact about them?")
+input ("What color are they?")
+print(animal + like + live + fact + color)
