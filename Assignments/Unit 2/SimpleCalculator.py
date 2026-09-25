b6784fdb7b8addddd7f82579814cf4264db24455
TadenWErdmann <827165@mystma.org>
2026-09-25 13:26:29 -0500

    ha

---
+++ Assignments/Unit 2/SimpleCalculator.py
@@ -0,0 +1,16 @@
+input("what calcuation do you want to run?")
+
+print(5 + 5)
+
+
+input("what calcuation do you want to run?")
+
+print(2)
+
+input("what calcuation do you want to run?")
+
+print(20)
+
+input("what calcuation do you want to run?")
+
+print(5)
